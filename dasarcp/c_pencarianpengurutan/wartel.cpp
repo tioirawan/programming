53311901c3a001ee2d8e954b80863bb6b00d165d
indmind <sattanationmail@gmail.com>
2022-12-10 21:15:48 +0700

    toki: wortel

--- dasarcp/c_pencarianpengurutan/wartel.cpp
+++ dasarcp/c_pencarianpengurutan/wartel.cpp
@@ -10,20 +10,14 @@ using namespace std;
 int main() {
   FAST;
 
-  // cout << strcmp("budi", "ani");
-  // cout << ("budi" > "ani");
-  // cout << (((string)"budi").compare("ani"));
-
   int n, q;
   cin >> n >> q;
 
-  vector<pair<string, int>> nums(n);
+  vector<pair<string, string>> nums(n);
 
   for (int i = 0; i < n; i++) {
-    string s;
-    cin >> s;
-    int num;
-    cin >> num;
+    string s;cin >> s;
+    string num;cin >> num;
     nums[i] = make_pair(s, num);
   }
 
@@ -39,19 +33,13 @@ int main() {
       int mid = (l + r) / 2;
       auto cur = nums[mid];
 
-      // cout << l << " " << r << " " << mid << endl;
-      // cout << sc << " " << cur.first << " " << sc.compare(cur.first) << endl;
-
       if (cur.first == sc) {
         cout << cur.second << endl;
         found = 1;
         break;
-      } else if (sc.compare(cur.first) < 0) {
-        // cout << "should be on the left\n";
+      } else if (sc < cur.first) {
         r = mid - 1;
       } else {
-        // cout << "should be on the right\n";
-
         l = mid + 1;
       }
     }
